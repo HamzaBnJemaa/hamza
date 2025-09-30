7eed7c0027ae5798d18b5e03bf6e4bf5de2f27b4
HamzaBnJemaa <benjemaahamza09@gmail.com>
2025-09-30 17:37:03 +0100

    55th

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,12 +1,34 @@
-import { Loader2 } from "lucide-react"
-
 export default function Loading() {
   return (
-    <div className="fixed inset-0 bg-black z-50 flex items-center justify-center">
-      <div className="text-center">
-        <Loader2 className="h-16 w-16 animate-spin text-blue-500 mx-auto mb-4" />
-        <p className="text-white text-lg font-medium">Loading...</p>
+    /* From Uiverse.io by Admin12121 */ 
+    <div className="loader">
+      <div className="box box0">
+        <div></div>
+      </div>
+      <div className="box box1">
+        <div></div>
+      </div>
+      <div className="box box2">
+        <div></div>
+      </div>
+      <div className="box box3">
+        <div></div>
+      </div>
+      <div className="box box4">
+        <div></div>
+      </div>
+      <div className="box box5">
+        <div></div>
+      </div>
+      <div className="box box6">
+        <div></div>
+      </div>
+      <div className="box box7">
+        <div></div>
+      </div>
+      <div className="ground">
+        <div></div>
       </div>
     </div>
-  )
+  );
 }
